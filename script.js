970dc70a57ca3b241c8a27cacf1981d9daffceb8
Akul-sada <sadananda.sherigara@gmail.com>
2022-12-24 19:53:25 +0530

    add photos of purses and bags

--- script.js
+++ script.js
@@ -6,8 +6,13 @@
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 const navEl = document.querySelectorAll(".main-nav-link");
-const link = 
+const price = document.querySelectorAll(".price");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+console.log(price);
+
+
+
+
 
