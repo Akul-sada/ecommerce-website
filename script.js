80fb9900672a28f64ec4709a29a49520428e7ce7
Akul-sada <sadananda.sherigara@gmail.com>
2022-12-24 15:38:31 +0530

    change navbar

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-
+const navEl = document.querySelectorAll(".main-nav-link");
+const link = 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
