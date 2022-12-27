c06bbcc539ff966dda641b4f9361bc3cb12cbf2b
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2022-12-27 10:54:34 +0530

    loop through navigation

--- navigationbar/script.js
+++ navigationbar/script.js
@@ -1,21 +1,16 @@
-const homeLink = document.getElementById('home-link');
-const bagLink = document.getElementById('bag-link');
-const storageLink = document.getElementById('storage-link');
+
 const menu = document.getElementById('menu');
 
-homeLink.addEventListener('click', () => {
-  menu.style.display = 'none';
-});
-
-bagLink.addEventListener('click', () => {
-  menu.style.display = 'none';
-});
-
-storageLink.addEventListener('click', () => {
-  menu.style.display = 'none';
-});
 const menuButton = document.getElementById('menu-button');
 
 menuButton.addEventListener('click', () => {
   menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
+
+const menuList = document.querySelectorAll("#menu ul li a");
+
+menuList.forEach(a => {
+  a.addEventListener("click", () => {
+    menu.style.display = "none";
+  });
 });
